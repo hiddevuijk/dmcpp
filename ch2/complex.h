4c098ed1bfa3ca4c94a97398bfbd0df6df837445
hiddevuijk <hdvuijk@gmail.com>
2019-04-14 10:49:27 +0200

    finished ch2

--- ch2/complex.h
+++ ch2/complex.h
@@ -1,26 +1,37 @@
 #ifndef guard_complex_h
 #define guard_complex_h
 
+#include <iostream>
+
 class complex
 {
   friend double abs(const complex&);
   public:
-	complex( double r, double i) : r(r), i(i) {}
-	complex( double r) : r(r) {}
-	complex() : r(0), i(0) {}
+	complex( double r=0, double i=0) : r(r), i(i) {}
+	//complex( const complex& c) : r(c.i), i(c.i) {}
 
-	double get_r() const { return r;}
-	double get_i() const { return i;}
-	void set( double rr, double ii)
-			{ r = rr;  i = ii;}
+	complex& operator=(const complex& src)
+		{ r= src.r; i=src.i; return *this; }
+
+	complex& operator=(const double& src)
+		{ r= src; i=0; return *this; }
+
+
+	const double& real() const  { return r; } 
+	const double& imag() const { return i; } 
+
   private:
 	double r,i;
 
 };
 
+
 double abs(const complex& c)
 { return c.r*c.r+c.i*c.i; }
 
-
+std::ostream& operator<<(std::ostream& os, const complex& c)
+{
+	return os << c.real() << " + " << c.imag() << "i";
+}
 
 #endif
